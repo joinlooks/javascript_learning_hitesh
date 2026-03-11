9c24da5c36abe13cc3d27b28e2e6258452116b46
Lakshya Kaushal Sharma <joinlooks@gmail.com>
2026-03-11 08:49:12 +0530

    Arrays done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,17 @@
+// var arr = ["lakshya", 234, true, null];
+// console.log(arr, typeof arr);
+// console.log("Hey");
+// printArray(arr);
+// function printArray(arr: string[]) {
+//     arr.forEach((e) => console.log(e));
+// }
+// let val: string = "string";
+// console.log(val);
+// val = 324;
+// console.log(val);
+
+// const value = Array.from(Object.values({ name: "Lakshya" }));
+// console.log(value);
+
+const value = Array.of(3, 4, 6);
+console.log(value);
---
+++ 02_basics/01_arrays.ts
@@ -0,0 +1,26 @@
+// const arr = ["lakshya", 234, true, null];
+// console.log(arr, typeof arr);
+
+// console.log("Hey");
+
+// printArray(arr);
+
+// function printArray(arr: string[]) {
+//     arr.forEach((e) => console.log(e));
+// }
+
+// let val: string = "string";
+// console.log(val);
+
+// val = 324;
+// console.log(val);
+
+// const arr: number[] = [3, 4];
+// arr.push(645);
+// // arr.push("lakshya");
+
+// console.log(arr.length, arr[2]);
+
+// const arr = [1, 2, 3, 4];
+// arr.push(5, 6);
+// console.log(arr);
